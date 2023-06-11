7a47bacb3999dc1f0bca341e3ceac32119eb4cdd
Dmitrii <dimasafanasyev@gmail.com>
2023-06-11 14:55:56 +0200

    first

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Menu from '../mehrmals/menu/Menu';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <Menu/>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/mehrmals/menu/Menu.js
@@ -0,0 +1,13 @@
+import './Menu.scss';
+
+const Menu = () => {
+
+
+    return (
+        <div className='title'>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+
+export default Menu;
